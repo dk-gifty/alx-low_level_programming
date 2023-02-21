de3f96172a9e1bc1524375b8765eaa32d05e3f22
dk-gifty <dikegift21@gmail.com>
2023-02-20 16:20:27 -0800

    task 5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,4 @@
-include <stdio.h>
+#include <stdio.h>
 /**
  *main - this is the main function
  *Return: always 0
@@ -6,6 +6,6 @@ include <stdio.h>
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("\"programming is like building a multilingual puzzle");
 	return (0);
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,13 @@
-include < stdio.h >
+#include <stdio.h>
+
 /**
- *main - this is the main function
- *Return: 0 when sucessful
+ * main - this is the main function
+ * 
+ * Return: 0 when sucessful
  */
 
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+		return (0);
 }
