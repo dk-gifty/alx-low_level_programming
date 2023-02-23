1f847e4fbb9c2db3975c6d3f3e643a1d2a3149bb
dk-gifty <dikegift21@gmail.com>
2023-02-22 19:39:49 -0800

    task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,11 +4,11 @@
  * Return: always 0
  */
 
-void print_alphabet(void);
+void print_alphabet(void)
 {
 	char i;
 
-	for (i + 'a' ; i <= 'z' ; i++)
+	for (i = 'a' ; i <= 'z' ; i++)
 		_putchar(i);
-	_putchar('\n')
+	_putchar('\n');
 }
