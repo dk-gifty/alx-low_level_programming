a46c1412ed072886fdad74ac66d00128191bbc7a
dk-gifty <dikegift21@gmail.com>
2023-02-24 19:36:14 -0800

    task 2

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isdigit - function to check for a digit
+ * @c: an integrar argument
+ * Return: success
+ */
+
+int _isdigit(int c)
+{
+	return (c >= '0' && c <= '9');
+}
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multiplying 2 numbers
+ * @a: integral param 1
+ * @b: integral param 2
+ * Return: success and intiger
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
